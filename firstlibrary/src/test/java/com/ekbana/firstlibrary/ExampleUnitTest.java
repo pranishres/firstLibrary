d83bc54f1e5e58aa4b94a2e43e43f74bee6e0851
Pranish Shrestha <pranish.shrestha@ekbana.info>
2019-06-10 16:34:24 +0545

    Add a Single<String> response function

--- firstlibrary/src/test/java/com/ekbana/firstlibrary/ExampleUnitTest.java
+++ firstlibrary/src/test/java/com/ekbana/firstlibrary/ExampleUnitTest.java
@@ -1,8 +1,9 @@
 package com.ekbana.firstlibrary;
 
+import io.reactivex.observers.DisposableSingleObserver;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,21 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getSingleResponseTest() {
+        MessageUtil util = new MessageUtil();
+        util.getSingleResponse().subscribe(new DisposableSingleObserver<String>() {
+            @Override
+            public void onSuccess(String value) {
+                System.out.println(value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
     }
 }
