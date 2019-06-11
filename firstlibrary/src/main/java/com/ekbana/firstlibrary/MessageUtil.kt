bd169a013274146d552b3d47ad6cd64f4b0aea77
Pranish Shrestha <pranish.shrestha@ekbana.info>
2019-06-11 10:57:51 +0545

    Configure bintray to publish library

--- firstlibrary/src/main/java/com/ekbana/firstlibrary/MessageUtil.kt
+++ firstlibrary/src/main/java/com/ekbana/firstlibrary/MessageUtil.kt
@@ -2,9 +2,14 @@ package com.ekbana.firstlibrary
 
 import android.content.Context
 import android.widget.Toast
+import io.reactivex.Single
 
 class MessageUtil {
     fun showToast(context: Context?, message: String?) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun getSingleResponse(): Single<String> {
+        return Single.just("Hello from other side")
     }
 }
